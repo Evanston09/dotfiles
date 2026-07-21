c57ba71f67b387de3862ebfa4a8b9324e2923c44
Evanston09 <evanhkim09@gmail.com>
2026-07-21 16:38:17 -0400

    Add old treesitter back since it is now maintained

--- nvim/lua/evank/plugins/treesitter.lua
+++ nvim/lua/evank/plugins/treesitter.lua
@@ -1,14 +1,46 @@
 return {
-  "romus204/tree-sitter-manager.nvim",
-  dependencies = {}, -- tree-sitter CLI must be installed system-wide
-  config = function()
-    require("tree-sitter-manager").setup({
-      -- Default Options
-      -- ensure_installed = {}, -- list of parsers to install at the start of a neovim session. If set to "all", install all parsers.
-      -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
-      auto_install = true, -- if enabled, install missing parsers when editing a new file
-      highlight = true, -- treesitter highlighting is enabled by default
-      -- languages = {}, -- override or add new parser sources
-    })
-  end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+
+    config = function()
+      local treesitter = require("nvim-treesitter")
+
+      treesitter.install({
+        -- Programming languages
+        "python",
+        "css",
+        "html",
+        "javascript",
+        "typescript",
+        "tsx",
+        "php",
+        "rust",
+        "go",
+        "cpp",
+        "c",
+
+        -- Configuration and project files
+        "dockerfile",
+        "yaml",
+        "gitignore",
+        "toml",
+
+        -- Useful Neovim/config parsers
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "*",
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end,
+  },
 }
+
